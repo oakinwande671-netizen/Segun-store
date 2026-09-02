c9fb6c3776dbdd595a1313015de79fed7d736060
Oluwasegun <oakinwande671@gmail.com>
2026-09-02 16:23:26 +0100

    i add a slash at the back of v1

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 import backendurl from "./backendurlsometinliketat";
 const api = axios.create({
-    baseURL: backendurl,
-    headers: {
+    baseURL:'https://segun-store-backend.onrender.com/api/v1',   
+     headers: {
         'Content-Type': 'application/json'
     }
 });
